f213ad9774a811749cf9ffcb4b920808ad9a78db
guest271314 <guest271314@gmail.com>
2014-04-12 14:00:31 -0700

    Create pseudo.js

---
+++ pseudo.js
@@ -0,0 +1,24 @@
+(function($) {
+/*!pseudo.js Copyright (c) 2014 guest271314 License BSD-2-Clause */  
+
+jQuery.fn.extend({
+    getPseudo : function (pseudo, prop ) {
+      var props = window.getComputedStyle (
+	  $(this).get(0), pseudo
+      ).getPropertyValue(prop);
+      return String(props);
+    },
+    setPseudo : function (_pseudo, _prop, newprop) {
+      var s = $("style");
+      var p = $(this).getPseudo(_pseudo, _prop);
+      var r = new RegExp(p);
+      return (r.test($(s).text()) 
+              ? $(s)
+                .text(function (index, prop) {
+                return prop.replace(r, newprop)
+                }) 
+              : prop 
+             );
+    }
+})
+})(jQuery);

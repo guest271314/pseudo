7e72b359342f4e0a03f92e72cd0bc8cbc60092e9
guest271314 <guest271314@gmail.com>
2014-12-02 18:14:08 -0800

    Update pseudo.js

--- pseudo.js
+++ pseudo.js
@@ -3,24 +3,36 @@
  * get, set css pseudo
  */  
 (function($) {
-jQuery.fn.extend({
-    getPseudo : function (pseudo, prop ) {
-      var props = window.getComputedStyle (
-	  $(this).get(0), pseudo
+  jQuery.fn.extend({
+    getPseudo: function(pseudo, prop) {
+      var props = window.getComputedStyle(
+        $(this).get(0), pseudo
       ).getPropertyValue(prop);
       return String(props);
     },
-    setPseudo : function (_pseudo, _prop, newprop) {
+    setPseudo: function(_pseudo, _prop, newprop) {
+      var elem = $(this);
       var s = $("style");
-      var p = $(this).getPseudo(_pseudo, _prop);
-      var r = new RegExp(p);
-      return (r.test($(s).text()) 
-              ? $(s)
-                .text(function (index, prop) {
-                return prop.replace(r, newprop)
-                }) 
-              : p
-             );
+      var p = elem.getPseudo(_pseudo, _prop);
+      var r = p !== "" ? new RegExp(p) : false;
+      var selector = $.map(elem, function(val, key) {
+        return [val.tagName
+                , val.id 
+                  ? "#" + val.id : null
+                , val.className ? "." + val.className : null]
+      });
+      var _setProp = "\n" + selector.join("")
+        .toLowerCase()
+        .concat(_pseudo)
+        .concat("{")
+        .concat(_prop + ":")
+        .concat(newprop + "};");
+      console.log(_setProp, p, r);
+      return ((!!r ? r.test($(s).text()) : r) ? $(s)
+        .text(function(index, prop) {
+          return prop.replace(r, newprop)
+        }) : $(s).append(_setProp)
+      );
     }
-})
+  })
 })(jQuery);
